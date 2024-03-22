831130e458e1135cb2f5a394b6a11ab212327f1b
James Pharis <jamesmpharis@gmail.com>
2024-03-22 00:15:37 -0400

    Believe I have completed the config service

--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+// import { BehaviorSubject } from 'rxjs';
+
 
 // interface Genres {
 //     genres: [
@@ -22,42 +23,41 @@ import { BehaviorSubject } from 'rxjs';
 
 export class configService{
 
-    
-    // private clueSource = new BehaviorSubject<boolean>(true);
-    // currentClueStatus = this.clueSource.asObservable();
-    public toggleClueStatus() {
-        localStorage.setItem('clueStatus', localStorage.getItem('clueStatus') === 'true' ? 'false' : 'true'); // if already true, change to false 
-        // this.clueSource.next(clueStatus);    // Do we still want to use Behavior Subjects? - remove later...
-    }
-    get clueStatus() { return localStorage.getItem('clueStatus') === 'true' ? true : false; }
-    // call this (in game.component.ts?) configService.clueStatus   - how to check clueStatus in game component
-
-
-    // private gamemodeSource = new BehaviorSubject<boolean>(false);
-    // currentGameMode = this.gamemodeSource.asObservable();
     public toggleGameMode() {
         localStorage.setItem('gameMode', localStorage.getItem('gameMode') === 'true' ? 'false' : 'true'); // if already true, change to false 
-        // this.gamemodeSource.next(gameMode);    // Do we still want to use Behavior Subjects? - remove later...
-    }
-    get gameMode() { return localStorage.getItem('gameMode') === 'true' ? true : false; }
-    // call this (in game.component.ts?) configService.gameMode   - how to check gameMode in game component
+    } // call  \/  this (in game.component.ts?) configService.gameMode   - how to check gameMode in game component
+    get gameMode() { return localStorage.getItem('gameMode') === 'true' ? true : false; } // return true if true, false if false
+            
+    public toggleClueStatus() {
+        localStorage.setItem('clueStatus', localStorage.getItem('clueStatus') === 'true' ? 'false' : 'true'); // if already true, change to false 
+    } // call  \/  this (in game.component.ts?) configService.clueStatus   - how to check clueStatus in game component
+    get clueStatus() { return localStorage.getItem('clueStatus') === 'true' ? true : false; } // return true if true, false if false
 
 
     // still just getting a string
-    public selectGenre() {
-        localStorage.setItem('genre', localStorage.getItem('genre') === 'true' ? 'false' : 'true');
+    public selectGenre(genre: string) {
+        localStorage.setItem('genre', genre);
     }
-    get selectedGenre() { return localStorage.getItem('genre') === 'true' ? true : false; }
+    get foundSelectedGenre() { 
+        return localStorage.getItem('genre'); 
+    }
 
-}  
+}
+// Use local storage if it is only being changed in once spot
+// if timed -> show timer logic... html
+
+
+    // Do we still want to use Behavior Subjects?   
+    // private gamemodeSource = new BehaviorSubject<boolean>(false);    
+    // currentGameMode = this.gamemodeSource.asObservable();    
+    // this.gamemodeSource.next(gameMode); 
+
+    // private clueSource = new BehaviorSubject<boolean>(true);         
+    // currentClueStatus = this.clueSource.asObservable();
+    // this.clueSource.next(clueStatus);    
 
     // private genreSource = new BehaviorSubject<Genres>({genres: ["rock","rap","pop","country","hip-hop","jazz","alternative","j-pop","k-pop","emo"]});
     // currentGenre = this.genreSource.asObservable();
-    // updateGenre(genre: Genres) {
+    // public updateGenre(genre: Genres) {
     //     this.genreSource.next(genre);
     // }
-
-
-// Use local storage if it is only being changed in once spot
-
-// if timed -> show timer logic... html
